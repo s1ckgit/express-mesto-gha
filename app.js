996b4e9baa60c055c3b4abe903288099328ba0a2
Kazzzemir <s1ckwell@mail.ru>
2023-10-04 13:18:19 +0300

    pr14 fixes

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { celebrate, Joi, errors } = require('celebrate');
-const { ValidationError, CastError, DocumentNotFoundError } = require('mongoose').Error;
+const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
-const UnathorizedError = require('./errors/Unathorized');
 const router = require('./routers/index');
-const { login, createUser } = require('./controllers/users');
-const {
-  BAD_REQUEST_CODE,
-  NOT_FOUND_CODE,
-  SERVER_ERROR_CODE,
-  ALREADY_EXIST_CODE,
-} = require('./data/responseStatuses');
-const auth = require('./middlewares/auth');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000 } = process.env;
 
@@ -23,52 +14,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(/https?:\/\/(www\.)?[\w-]+\.\w+(\/.+)?/i),
-  }),
-}), createUser);
 app.use((req, res, next) => {
   if (req.cookies.jwt) {
     req.headers.authorization = `Bearer ${req.cookies.jwt}`;
   }
   next();
 });
-app.use(auth);
-app.use((req, res, next) => {
-  next();
-});
 app.use('/', router);
-app.use('*', (req, res) => {
-  res.status(NOT_FOUND_CODE).send({ message: 'Данная страница не найдена' });
-});
 app.use(errors());
-app.use((err, req, res, next) => {
-  if (err instanceof CastError) {
-    res.status(BAD_REQUEST_CODE).send({ message: 'Проверьте корректность введённых данных' });
-  } else if (err instanceof DocumentNotFoundError) {
-    res.status(NOT_FOUND_CODE).send({ message: 'Данные не были найдены' });
-  } else if (err instanceof ValidationError) {
-    res.status(BAD_REQUEST_CODE).send({ message: 'Ошибка валидации, проверьте корректность данных' });
-  } else if (err instanceof UnathorizedError) {
-    res.status(err.statusCode).send({ message: err.message });
-  } else if (err.code === 11000) {
-    res.status(ALREADY_EXIST_CODE).send({ message: 'Такой пользователь уже существует' });
-  } else {
-    res.status(SERVER_ERROR_CODE).send({ message: 'Произошла ошибка на стороне сервера' });
-  }
-  next();
-});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log('Полёт нормальный');
--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const { createUser, login } = require('../controllers/users');
+const auth = require('../middlewares/auth');
+const signInValidation = require('../middlewares/signInValidation');
+const signUpValidation = require('../middlewares/signUpValidation');
+const NotFoundError = require('../errors/NotFound');
 
+router.post('/signin', signInValidation, login);
+router.post('/signup', signUpValidation, createUser);
+router.use(auth);
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+router.use('*', (req, res, next) => {
+  next(new NotFoundError());
+});
 
 module.exports = router;
